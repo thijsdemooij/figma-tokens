360fcba0af2a3955ea9e6f25e528f1261623958f
thijsdemooij <thijsdemooij@users.noreply.github.com>
2024-02-18 21:38:08 +0000

    Commit from GitHub Actions (CI)

---
+++ build/compose/StyleDictionaryColor.kt
@@ -0,0 +1,16 @@
+
+
+// Do not edit directly
+// Generated on Sun, 18 Feb 2024 21:38:07 GMT
+
+
+
+package StyleDictionaryColor;
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.*
+
+object StyleDictionaryColor {
+  val colorShadowOpacityBase = Color(0xff000000)
+  val colorShadowOpacityBlur = Color(0xff000000)
+}
